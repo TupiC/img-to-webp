9b8c1f420c3dee296a49ca7ea01daec308d1534c
Christoph Tupi <ctupi.mmt-b2021@fh-salzburg.ac.at>
2025-01-26 14:57:17 +0100

    feat: allow input folder to have sub dirs

--- convert.sh
+++ convert.sh
@@ -2,17 +2,19 @@
 
 OUTPUT_FOLDER="/output"
 
-
-for IMAGE in /input/*.{jpg,jpeg,png,gif,bmp,tiff,tif}; do
+find /input -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" -o -iname "*.bmp" -o -iname "*.tiff" -o -iname "*.tif" \) | while read IMAGE; do
     [ -e "$IMAGE" ] || continue
 
+    RELATIVE_PATH="${IMAGE#/input/}"
+    OUTPUT_PATH="$OUTPUT_FOLDER/$(dirname "$RELATIVE_PATH")"
     FILENAME=$(basename "$IMAGE")
     BASENAME="${FILENAME%.*}"
 
-    cwebp -q 80 "$IMAGE" -o "$OUTPUT_FOLDER/$BASENAME.webp"
+    mkdir -p "$OUTPUT_PATH"
+    cwebp -q 80 "$IMAGE" -o "$OUTPUT_PATH/$BASENAME.webp"
 
     if [ $? -eq 0 ]; then
-        echo "Converted: $IMAGE -> $OUTPUT_FOLDER/$BASENAME.webp"
+        echo "Converted: $IMAGE -> $OUTPUT_PATH/$BASENAME.webp"
     else
         echo "Failed to convert: $IMAGE"
     fi
